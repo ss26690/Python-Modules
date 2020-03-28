6c7195c7375c7f6efab22f9ece868ff1a1ced36e
Saurav Sagar <37689450+ss26690@users.noreply.github.com>
2020-03-28 23:49:19 +0530

    Add files via upload

---
+++ String/string2.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+str1 = "Hello"
+str2 = "Guys"
+
+
+# In[3]:
+
+
+print(str1+", "+str2+"!")
+
+
+# In[4]:
+
+
+print(str1*3)
+
+
+# In[5]:
+
+
+print(str2[3])
+
+
+# In[6]:
+
+
+print(str2[1:4])
+
+
+# In[7]:
+
+
+print("y" in str2)
+
+
+# In[8]:
+
+
+print("a" in str2)
+
+
+# In[9]:
+
+
+print("Y" in str2)
+
+
+# In[ ]:
+
+
+
+
